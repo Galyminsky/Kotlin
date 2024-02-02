cdfc53404048844960da0c1126a16c9b2ca9b7c4
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-02 14:03:08 +0600

    1

--- src/main/kotlin/Day_2/Operator.kt
+++ src/main/kotlin/Day_2/Operator.kt
@@ -2,15 +2,15 @@ package Day_2
 
 fun main() {
 
-    val hasDriverLicense = false
-    val isCarWorking = false
-    val isPublicTransportAvailable = false
-    if (hasDriverLicense && isCarWorking && (!isPublicTransportAvailable) ) {
-        println("Car")
-    } else if ((!hasDriverLicense || !isCarWorking) && isPublicTransportAvailable) {
-        println("PublicTransport")
-    } else if ((!hasDriverLicense) && (!isCarWorking) && (!isPublicTransportAvailable)) {
-        println("Way")
-    }
+   val sleepDuration = 5
+   val hasDreams = false
+   val wokeUpNaturally = true
+   if (sleepDuration in 5..8 && hasDreams && wokeUpNaturally) {
+      println("Good Dreams")
+   } else if (sleepDuration in 5..8 && (!hasDreams || !wokeUpNaturally)) {
+      println("Bad Dreams")
+   } else if (sleepDuration in 1..5 && hasDreams && wokeUpNaturally) {
+      println("Fuck Dreams")
+   }
 
 }
