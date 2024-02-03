cb3956b7b57d9f971e19b4e188e0420f98a3d23b
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-03 12:50:26 +0600

    2

--- src/main/kotlin/Day_2/Operator.kt
+++ src/main/kotlin/Day_2/Operator.kt
@@ -2,15 +2,50 @@ package Day_2
 
 fun main() {
 
-   val sleepDuration = 5
-   val hasDreams = false
-   val wokeUpNaturally = true
-   if (sleepDuration in 5..8 && hasDreams && wokeUpNaturally) {
-      println("Good Dreams")
-   } else if (sleepDuration in 5..8 && (!hasDreams || !wokeUpNaturally)) {
-      println("Bad Dreams")
-   } else if (sleepDuration in 1..5 && hasDreams && wokeUpNaturally) {
-      println("Fuck Dreams")
-   }
+    // Task 1
+    val temperature = 30
+    val isSunny = true
+    if (temperature in 20..30 && isSunny) {
+        println("Отличная погода для активного отдыха")
+    }else if ((temperature <= 20) || (temperature >= 30) || !isSunny) {
+        println("Погода не подходит...")
+    }
+
+    // Task 2
+    val numberOfDishes = 4
+    val pricePerDish = 300
+    val resultPrice = numberOfDishes * pricePerDish
+    println(resultPrice)
+
+    // Task 3
+    val isCompiled = false
+    val isHighLevel = false
+    val isScripting = true
+    if (isCompiled && isHighLevel && isScripting) {
+        println("It's is my PL")
+    }else if (!isCompiled || !isHighLevel && !isScripting) {
+        println("It's NOT my PL")
+    }
+
+    // Task 4
+    val height = 175
+    val currentWeight = 80.0F
+    val idealWeight = (height - 100) * 1.15
+    println(idealWeight)
+
+    // Task 5
+    val userName = "Lisander"
+    val userAge = 55
+    val welcomeTeen = "Добро пожаловать в мир программирования!"
+    val welcomeMan = "Рад снова видеть тебя!"
+    val welcomeDad = "Ты еще жив, старичек?"
+    val privet = "Привет, "
+    if (userAge <= 18) {
+        println("$privet $userName $welcomeTeen")
+    } else if (userAge in 18..40) {
+        println("$privet $userName $welcomeMan")
+    }else if (userAge in 40..100) {
+        println("$privet $userName $welcomeDad")
+    }
 
 }
