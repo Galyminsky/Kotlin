f47897d6deffdc9b9a63b69f2e4bfbb1ccf84bf9
Galyaminsky_Linux <jobforandroid@proton.me>
2024-02-03 17:52:34 +0600

    Refactor

--- src/main/kotlin/Day_2/Calculator.kt
+++ src/main/kotlin/Day_2/Calculator.kt
@@ -1,15 +1,30 @@
 package Day_2
 
 fun main() {
-    println("Введите число для таблицы умножения:")
-    val number = readlnOrNull()?.toInt()
-    if (number != null) {
-        println("\n Таблица умноженения для числа: $number \n")
-        for (i in 1..10) {
-            val result = i * number
-            println("$i * $number = $result")
-        }
+    println("Введите первое число:")
+    val numberOne = readlnOrNull()?.toIntOrNull() ?: 0
+    println("Введите знак действия:")
+    val action = readlnOrNull()
+    println("Введите второе число:")
+    val numberTwo = readlnOrNull()?.toIntOrNull() ?: 0
+
+    val result = when (action) {
+        "+" -> numberOne + numberTwo
+        "-" -> numberOne - numberTwo
+        "*" -> numberOne * numberTwo
+        "/" -> numberOne / numberTwo
+        else -> null
+    }
+
+    if (result != null) {
+        println("Result: $result")
     } else {
         println("Error")
     }
+
 }
+fun readlnOrNull(): String? {
+    return kotlin.io.readlnOrNull()
+}
+
+
